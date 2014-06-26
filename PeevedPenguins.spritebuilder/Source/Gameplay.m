eea307ea34651abffb87b5fba9c93fb5b9474f2a
JoshuaSani <joshuasani@Joshuas-MacBook-Pro.local>
2014-06-26 16:03:54 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -7,6 +7,9 @@
 //
 
 #import "Gameplay.h"
+#import "Penguin.h"
+
+static const float MIN_SPEED = 5.f;
 
 @implementation
 
@@ -23,8 +26,10 @@ Gameplay{
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
     
-    CCNode *_currentPenguin;
+    Penguin *_currentPenguin;
     CCPhysicsJoint *_penguinCatapultJoint;
+    
+    CCAction *_followPenguin;
 }
 
 // is called when CCB file has completed loading
@@ -61,7 +66,7 @@ Gameplay{
     }
     
     // create a penguin from the ccb-file
-    _currentPenguin = [CCBReader load:@"Penguin"];
+    _currentPenguin = (Penguin*)[CCBReader load:@"Penguin"];
     // initially position it on the scoop. 34, 138 is the position in the node spaceof the _catapultArm
     CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
     // transform the world position to the node space to which the penguin will be added (_physicsNode)
@@ -99,8 +104,10 @@ Gameplay{
     _currentPenguin.physicsBody.allowsRotation = TRUE;
     
     // follow the flying penguin
-    CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
-    [_contentNode runAction:follow];
+    _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+    [_contentNode runAction:_followPenguin];
+    
+    _currentPenguin.launched = TRUE;
 }
 
 -(void) touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
@@ -168,5 +175,39 @@ Gameplay{
     [seal removeFromParent];
 }
 
+- (void)nextAttempt {
+    _currentPenguin = nil;
+    [_contentNode stopAction:_followPenguin];
+    
+    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(0,0)];
+    [_contentNode runAction:actionMoveTo];
+}
+
+
+
+- (void)update:(CCTime)delta
+{
+    if (_currentPenguin.launched) {
+        // if speed is below minimum speed, assume this attempt is over
+        if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
+            [self nextAttempt];
+            return;
+        }
+        
+        int xMin = _currentPenguin.boundingBox.origin.x;
+        
+        if (xMin < self.boundingBox.origin.x) {
+            [self nextAttempt];
+            return;
+        }
+        
+        int xMax = xMin+ _currentPenguin.boundingBox.size.width;
+        
+        if (xMax > ( self.boundingBox.origin.x + self.boundingBox.size.width)) {
+            [self nextAttempt];
+            return;
+        }
+    }
+}
 
 @end
