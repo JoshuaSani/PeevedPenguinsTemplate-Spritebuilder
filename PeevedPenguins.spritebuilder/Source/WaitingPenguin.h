04caa93b6b7e56f2a33fe14120f16b6952f8a716
JoshuaSani <joshuasani@Joshuas-MacBook-Pro.local>
2014-06-26 15:35:03 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Joshua Sani on 26/6/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
